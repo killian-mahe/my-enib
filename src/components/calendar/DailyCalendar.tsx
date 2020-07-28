4cbc6af0dd08e6e0a97ba4f3de85e36ec8e0eaba
Killian Mahé <killianmahe.pro@gmail.com>
2020-07-28 16:28:54 +0200

    Create Events Shapes

---
+++ src/components/calendar/DailyCalendar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './DailyCalendar.css';
+import CalendarEvent from '../../models/CalendarEvent';
+
+interface CalendarProps {
+
+};
+
+const DailyCalendar : React.FC<CalendarProps> = () => {
+
+    let events = [
+        new CalendarEvent(new Date(2019, 2, 1, 8, 0), new Date(2019, 2, 1, 20, 0))
+    ];
+
+    let renderedEvents = events.map(function(event){
+        let size = (event.stop.getTime() - event.start.getTime())/(1000*60); // Difference in minutes
+        let percentage = size*100/(12*60);
+
+        return <div className="calendar-event" style={{height: percentage+"%"}}></div>
+    });
+
+    return (
+        <div className="daily-calendar">
+            {renderedEvents}
+        </div>
+    );
+};
+
+export default DailyCalendar;
--- src/pages/Agenda.tsx
+++ src/pages/Agenda.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonSegment, IonSegmentButton } from '@ionic/react';
 import { calendarOutline } from 'ionicons/icons';
 import './Agenda.css';
+import DailyCalendar from '../components/calendar/DailyCalendar';
 
 const Agenda: React.FC = () => {
   return (
+    
     <IonPage>
       <IonContent>
-        <IonHeader collapse="condense" mode="ios">
+        <IonHeader>
           <IonToolbar>
             <IonTitle><IonIcon icon={calendarOutline} /> Agenda</IonTitle>
           </IonToolbar>
         </IonHeader>
+        
+        <IonToolbar>
+          <IonSegment value="all">
+            <IonSegmentButton value="all">Jour</IonSegmentButton>
+            <IonSegmentButton value="favorites">Semaine</IonSegmentButton>
+          </IonSegment>
+        </IonToolbar>
+
+        <DailyCalendar />
       </IonContent>
     </IonPage>
   );
