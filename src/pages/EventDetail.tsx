b8c0849b1e9e2203325487ca3a36a5764afb8a0c
Killian Mahé <killianmahe.pro@gmail.com>
2020-07-30 16:52:11 +0200

    💄 EventDetail Page

--- src/pages/EventDetail.tsx
+++ src/pages/EventDetail.tsx
@@ -31,10 +31,39 @@ function EventDetail() {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-
+                <div className="p-5">
+                    <div className="text-2xl justify-center font-sans font-medium">{event?.course[0].name}</div>
+                    <div className="grid grid-cols-1 mt-3 divide-y divide-gray-300">
+                        <Row attribute="Professeur" value={event?.course[0].professor}/>
+                        <Row attribute="Salle" value={event?.course[0].classRoom}/>
+                        <Row attribute="Durée" value={_getDuration(event)}/>
+                    </div>
+                </div>
             </IonContent>
         </IonPage>
     );
+}
+
+interface RowProps {
+    attribute: string;
+    value: string | undefined;
+}
+
+function Row(props: RowProps) {
+    return (
+        <div className="py-3 grid grid-cols-2">
+            <div className="font-semibold font-sans">{props.attribute}</div>
+            <div className="text-right">{props?.value}</div>
+        </div>
+    );
+}
+
+function _getDuration(event: CalendarEvent | undefined) {
+    if (!event) return undefined;
+
+    let duration = event.stop.getTime() - event.start.getTime();
+    duration /= (1000*60);
+    return duration > 100 ? `${Math.round((duration/60))} h` : `${Math.round(duration)} min`;
 }
 
 export default EventDetail;
