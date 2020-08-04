87226fa48757fadfaa04ef7239c75ff0bd7d2cfa
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-04 16:34:09 +0200

    🍱 Update Slide illustration

--- src/pages/slides/Slide2.tsx
+++ src/pages/slides/Slide2.tsx
@@ -5,8 +5,10 @@ import { calendarSharp } from 'ionicons/icons';
 function Slide2() {
 
     return (
-        <IonSlide className="flex flex-col p-4">
-            <IonIcon icon={calendarSharp} className="text-6xl mb-4 animate-bounce-slow" />
+        <IonSlide className="flex flex-col p-4 items-center pb-20">
+            <div className="w-2/3 mb-10 animate-bounce-slow">
+                <img src={process.env.PUBLIC_URL + "/assets/slides/undraw_events_2p66.svg"}/>
+            </div>
             <h1 className="text-blue-600 text-3xl font-sans font-bold mb-6">Ton emploi du temps</h1>
             <p className="text-lg font-sans font-light mt-4">Une interface simple pour consulter ton emploi du temps, même hors-connection !</p>
         </IonSlide>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'bounce-slow': 'bounce 3s infinite',
+        'bounce-slow': 'bounce 5s infinite',
       },
       borderRadius: {
         'xl': '3.5rem'
