bf8c870ff94eb1611485a12d30eb85fca7c42289
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-06 13:55:40 +0200

    ✨ Adding Weekly Events

--- src/components/calendar/weeklyCalendar/components/DayCol.tsx
+++ src/components/calendar/weeklyCalendar/components/DayCol.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CalendarEvent from '../../../../models/CalendarEvent';
+import { WeeklyEvent } from './WeeklyEvent';
 
 interface DayProps {
     hours: number;
@@ -15,12 +16,30 @@ export function DayCol({hours, className, events}: DayProps) {
     }
 
     return (
-        <div className={`h-full flex flex-col ${className}`}>
+        <div className={`h-full flex flex-col relative ${className}`}>
             {
                 hoursMap.map((hour) => {
                     return <div className="flex-1 w-full border-l border-t border-solid border-gray-200" key={hour}></div>
                 })
             }
+            {
+                events?.map((event) => {
+                    return <WeeklyEvent event={event} style={{'position': 'absolute', 'top': _getPosition(event, hoursMap), 'height': _getHeight(event, hoursMap)}}/>
+                })
+            }
         </div>
     );
+}
+
+function _getPosition(event: CalendarEvent, hours: number[]) : string {
+    const start = new Date(event.start.getTime() - 8*60*60*1000);
+    let percent = (start.getHours()*60 + start.getMinutes()) * 100 / (hours.length*60);
+    return `${percent}%`;
+}
+
+function _getHeight(event: CalendarEvent, hours: number[]): string {
+    const period = Math.round((event.stop.getTime() - event.start.getTime()) / 60000);
+
+    let percent = period * 100 / (hours.length*60);
+    return `${percent}%`;
 }
---
+++ src/components/calendar/weeklyCalendar/components/WeeklyEvent.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { IonAlert } from '@ionic/react';
+import CalendarEvent from '../../../../models/CalendarEvent';
+
+interface WeeklyEvent {
+    event: CalendarEvent;
+    style?: React.CSSProperties;
+    className?: string;
+}
+
+export function WeeklyEvent({event, style, className}: WeeklyEvent) {
+
+    const [showAlert, setShowAlert] = useState(false);
+
+    return (
+        <>
+            <div className={`w-full p-1 ${className}`} style={style} onClick={() => setShowAlert(true)}>
+                <div className="bg-blue-500 hover:bg-blue-700 rounded w-full h-full flex flex-col content-around justify-center">
+                    <span className="uppercase text-white font-sans font-bold text-sm">{_getAbbrName(event.course[0].name)}</span>
+                    <span className="text-white font-sans text-2xs">{event.course[0].classRoom}</span>
+                </div>
+            </div>
+            <IonAlert
+                    isOpen={showAlert}
+                    onDidDismiss={() => setShowAlert(false)}
+                    cssClass='my-custom-class'
+                    header={event.course[0].name}
+                    subHeader={event.course[0].professor}
+                    message={'This is an alert message.'}
+                    buttons={['OK']}
+                />
+        </>
+    );
+}
+
+function _getAbbrName(name: string): string {
+    let splitedName = name.split(' ');
+    let abbrName = "";
+
+    if (splitedName.length > 1) {
+        splitedName.forEach(e => {
+            abbrName += e.slice(0, 1);
+        });
+    } else {
+        abbrName = name.slice(0, 3);
+    }
+
+    return abbrName;
+}
