6b85e0ecb5aca408ed17766812b20ddb52e4b3c5
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-06 09:38:58 +0200

    💄 Updating Weekly Calendar Header

--- src/components/calendar/weeklyCalendar/components.tsx
+++ src/components/calendar/weeklyCalendar/components.tsx
@@ -16,6 +16,10 @@ interface WeekProps {
     startDay: Date;
 }
 
+interface DayHeaderProps {
+    day: Date;
+}
+
 export function HourRow({label, className} : HourRowProps) {
 
     return (
@@ -43,9 +47,23 @@ export function DayCol({hours, className}: DayProps) {
     );
 }
 
-export function Week({hours, startDay}: WeekProps) {
+export function DayHeader({ day }: DayHeaderProps) {
 
     const weekDays = ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'];
+    const now = new Date(Date.now());
+
+    let color = "";
+    if (day.toDateString() === now.toDateString()) color = "text-white bg-blue-600"
+
+    return (
+        <div className="flex-1 flex flex-col h-20 justify-center font-sans text-sm items-center" key={day.getTime()}>
+            <span className="font-light">{weekDays[day.getDay() - 1]}</span>
+            <div className={`flex justify-center items-center font-bold h-6 w-6 rounded-full ${color}`}>{day.getDate()}</div>
+        </div>
+    );
+}
+
+export function Week({hours, startDay}: WeekProps) {
 
     let days: Date[] = [];
 
@@ -59,12 +77,7 @@ export function Week({hours, startDay}: WeekProps) {
                     <div className="w-1/7" />
                     {
                         days.map((day) => {
-                            return (
-                                <div className="flex-1 flex flex-col h-20 justify-center font-sans text-sm items-center" key={day.getTime()}>
-                                    <span className="font-light">{weekDays[day.getDay() - 1]}</span>
-                                    <div className="flex justify-center items-center font-bold text-white bg-blue-600 h-6 w-6 rounded-full">{day.getDate()}</div>
-                                </div>
-                            );
+                            return <DayHeader day={day}/>
                         })
                     }
                 </div>
