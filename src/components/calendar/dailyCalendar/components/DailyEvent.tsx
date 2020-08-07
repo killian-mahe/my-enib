7ddfcf17376bac7f8ad9c2d439471fd8c3bf90c5
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-07 16:45:17 +0200

    💄 Change Daily Event Appearence on stage changed

--- src/components/calendar/dailyCalendar/components/DailyEvent.tsx
+++ src/components/calendar/dailyCalendar/components/DailyEvent.tsx
@@ -17,7 +17,7 @@ export enum EventState {
 function DailyEvent(props: DailyEventProps) {
 
     return (
-            <div className="rounded overflow-hidden bg-white w-full">
+            <div className={`rounded overflow-hidden bg-white w-full ${props.state === EventState.now ? 'border-blue-700 border-2' : ''} ${props.state === EventState.passed ? 'opacity-75' : ''}`}>
                 <div className="px-3 py-2">
                     <div className="font-bold text-md mb-2 items-center flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-6 w-6 text-gray-500 mr-1" stroke="currentColor">
