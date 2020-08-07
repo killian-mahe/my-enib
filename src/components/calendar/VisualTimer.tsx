e424f9d4f8c50403d10b7c3bc6ba5655cfe94eca
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-07 14:09:06 +0200

    💄 Change VisualTimer Icon

--- src/components/calendar/VisualTimer.tsx
+++ src/components/calendar/VisualTimer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { now } from '../../App';
+import { BellIcon, CheckIcon, ClockIcon } from '../Icons';
 
 interface VisualTimerProps {
     start: number;
@@ -10,9 +11,6 @@ function VisualTimer(props: VisualTimerProps) {
     
     return (
     <div className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 items-center flex">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-4 w-4 mr-1" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
-        </svg>
         {_getWaitTime(props.start, props.stop)}
     </div>
     );
@@ -25,12 +23,12 @@ function _getWaitTime(start: number, stop: number) {
     pendingTime = Math.round(pendingTime);
     if (pendingTime < 0) {
         if (Math.abs(pendingTime) < duration) {
-            return "En cours";
+            return <>{BellIcon} En cours</>;
         } else {
-            return "Done";
+            return <>{CheckIcon} Done</>;
         }
     } else {
-        return (pendingTime > 60) ? Math.round(pendingTime/60) + " h" : Math.round(pendingTime) + " min";
+        return <>{ClockIcon} {(pendingTime > 60) ? Math.round(pendingTime/60) + " h" : Math.round(pendingTime) + " min"}</>
     }
 }
 
