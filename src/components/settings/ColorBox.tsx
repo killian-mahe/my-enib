6c5147570f7e350d9a3db75f622e19d6b872e0dd
Killian Mahé <killianmahe.pro@gmail.com>
2020-08-13 11:39:39 +0200

    🚸 Adding ColorBox component

---
+++ src/components/settings/ColorBox.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { TwitterPicker } from 'react-color';
+
+interface ColorBoxProps {
+    color: string;
+    onClick?(): void;
+}
+
+function ColorBox(props: ColorBoxProps) {
+
+    const [displayColorPicker, setDisplayColorPicker] = useState<boolean>(false);
+    const [color, setColor] = useState<string>(props.color);
+
+    const handleOnClick = () => {
+        setDisplayColorPicker(!displayColorPicker);
+    } 
+    
+    const handleOnClose = () => {
+        setDisplayColorPicker(false);
+    }
+
+    const handleOnChange = (color: any, event: any) => {
+        setColor(color.hex);
+    }
+
+    return (
+        <>
+        <div className="h-6 w-6 rounded static" style={{'backgroundColor': color}} onClick={handleOnClick}/>
+        { displayColorPicker ? <div className="absolute right-0 top-full z-10">
+            <div className="fixed inset-0" onClick={handleOnClose}/>
+            <TwitterPicker color={color} triangle="top-right" onChange={handleOnChange}/>
+        </div> : null }
+        </>
+    );
+}
+
+export default React.memo(ColorBox);
